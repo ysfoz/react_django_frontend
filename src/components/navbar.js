6c39029490ae5bd03f8527bd32513097157e03c5
yusuf <ysfozturk3@gmail.com>
2021-01-24 22:41:27 +0100

    added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import Avatar from '@material-ui/core/Avatar';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import logo from '../assets/dj.png'
+import logo2 from '../assets/react.png'
+
+
+
+const useStyles = makeStyles((theme) => ({
+   font:{
+    fontSize:10,
+    marginLeft:2,
+    marginLeft: theme.spacing(-1),
+   },
+   small: {
+    width: theme.spacing(5),
+    height: theme.spacing(5),
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+   
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    }
+}
+
+  
+}));
+
+
+export default function NavBar() {
+const classes = useStyles();
+const [anchorEl, setAnchorEl] = React.useState(null);
+const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+ 
+  const matches = useMediaQuery('(min-width:750px)');
+
+  return (
+    <div className={classes.root}>
+   
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            
+          {/* <img src = {logo2} style={{width:50, height:50}}/> */}
+          <Avatar alt="Remy Sharp" src={logo} className={classes.small} />
+          {/* <img src = {logo} style={{width:50, height:50}}/> */}
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+          Blog
+          </Typography>
+
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+
+          <Button color="inherit" className={matches ? null :classes.font}>Login</Button>
+          <Button color="inherit" className={matches ? null :classes.font}>Register</Button>
+          <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
