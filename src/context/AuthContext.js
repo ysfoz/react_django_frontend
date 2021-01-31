86ed2faab4152fde0cc9ac6844dc24c2f1f1def1
yusuf <ysfozturk3@gmail.com>
2021-01-31 17:10:47 +0100

    added context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -8,23 +8,30 @@ export const AuthContext = createContext();
 
 function AuthContextProvider(props) {
   const [postList, setPostList] = useState();
+  const [pageCount, setPageCount] = useState()
+
   // const [currentUser, setCurrentUser] = useState(null);
   const [currentUser, setCurrentUser] = useState(null);
 
-  const fetchDataList = async (data) => {
-    const res = await axios.get('https://blog-backend-ysf.herokuapp.com/list/', data)
-    return res?.data
+  const fetchDataList = async (num) => {
+    const res = await axios.get('https://blog-backend-ysf.herokuapp.com/list/')
+    const items = res?.data
+    const page = res?.data?.filter((item,i)=> i+1 <=6 * num && i >=(num-1) * 6)
+    setPageCount(Math.ceil(items?.length/6))
+    setPostList(num ? page : items)
+    
   }
 
   const fetchDataLogin = async (path, data) => {
     const res = await axios.post(path, data)
     return res?.data
+    
   }
 
 
   
   return (
-    <AuthContext.Provider value={{ postList, setPostList, fetchDataList, setCurrentUser, currentUser, fetchDataLogin }}>
+    <AuthContext.Provider value={{ postList, setPostList, fetchDataList, setCurrentUser, currentUser, fetchDataLogin, pageCount }}>
       {props.children}
     </AuthContext.Provider>
   );
