9eceed125434ddd809a11151b5c0bf4ec4b69df5
yusuf <ysfozturk3@gmail.com>
2021-01-31 17:11:46 +0100

    solved bug

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
@@ -63,7 +63,7 @@ export default function BlogCard({
   viewCount,
 }) {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -125,7 +125,9 @@ export default function BlogCard({
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
           })}
-          onClick={handleExpandClick}
+          onClick={
+            handleExpandClick 
+          }
           aria-expanded={expanded}
           aria-label="show more"
         >
